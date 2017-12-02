c62cd7ee1afe7844650e8cc8ece036541cdbceb3
Michael Grand <michael.grand.2@gmail.com>
2017-12-02 11:24:57 -0800

    Tiled2Unity package imported. "Medium" test level created.

---
+++ Maps/Tilesets/Tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <image source="../../Assets/Artwork/Wall.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+</tileset>
