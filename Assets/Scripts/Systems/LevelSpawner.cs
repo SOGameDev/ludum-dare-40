3bf88c5ed3b81848c0ee65cff499910b99c80504
Michael Grand <michael.grand.2@gmail.com>
2017-12-02 11:58:54 -0800

    Created level spawner.

---
+++ Assets/Scripts/Systems/LevelSpawner.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Systems
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    ///     Handles spawning the level.
+    /// </summary>
+    public class LevelSpawner : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameObject levelPrefab = Resources.Load("Levels/Medium") as GameObject;
+            Instantiate(levelPrefab);
+        }
+    }
+}
