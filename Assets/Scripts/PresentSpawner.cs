a16d32f4e7d89ebc2c2d0e825e58b9b6e2f8774a
Michael Grand <michael.grand.2@gmail.com>
2017-12-02 18:57:17 -0800

    Presents are now spawned.

--- Assets/Scripts/PresentSpawner.cs
+++ Assets/Scripts/PresentSpawner.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace Assets.Scripts
+{
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class PresentSpawner : MonoBehaviour {
+    /// <summary>
+    ///     Spawns presents. This is the present factory. The doorway to Santa's Workshop. The exit pipe of all the wonder and joy that elvish crafters...  Craft.
+    /// </summary>
+    public class PresentSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        List<Present> presentPrefabs;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [SerializeField]
+        float minSpawnTime = 0.5f;
+
+        [SerializeField]
+        float maxSpawnTime = 1.0f;
+
+        float lastSpawnTime;
+
+        float spawnTime;
+
+        void Start()
+        {
+            lastSpawnTime = Time.time;
+            spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        }
+
+        void Update()
+        {
+            if (presentPrefabs.Count <= 0) return;
+
+            if (Time.time >= lastSpawnTime + spawnTime)
+            {
+                int presentIndex = Random.Range(0, presentPrefabs.Count - 1);
+                Instantiate(presentPrefabs[presentIndex], transform.position, Quaternion.Euler(0, 0, Random.Range(0.0f, 360.0f)));
+                lastSpawnTime = Time.time;
+            }
+        }
+    }
 }
