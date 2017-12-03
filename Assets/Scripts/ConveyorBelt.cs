c61a6227f57b1ed4487961256af365c938a7d028
Michael Grand <michael.grand.2@gmail.com>
2017-12-02 17:24:44 -0800

    Conveyor belt custom script.

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,24 @@
+namespace Assets.Scripts
+{
+    using UnityEngine;
+    using System.Collections;
+
+    /// <summary>
+    ///     Conveyor belt.
+    /// </summary>
+    public class ConveyorBelt : MonoBehaviour
+    {
+        [SerializeField]
+        float speed = 5;
+
+        /// <summary>
+        ///     The speed that this conveyor belt is moving at.
+        /// </summary>
+        /// <value>The speed.</value>
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+    }
+}
---
+++ Assets/Scripts/Present.cs
@@ -0,0 +1,52 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    /// <summary>
+    ///     Misc. present logic.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Present : MonoBehaviour
+    {
+        const string conveyorBeltTag = "Conveyor Belt";
+
+        bool onConveyorBelt;
+
+        new Rigidbody2D rigidbody;
+
+        Vector2 conveyorBeltDirection;
+
+        float conveyorBeltSpeed;
+
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag(conveyorBeltTag))
+            {
+                onConveyorBelt = true;
+                conveyorBeltDirection = collision.transform.right;
+                conveyorBeltSpeed = collision.GetComponent<ConveyorBelt>().Speed;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag(conveyorBeltTag))
+            {
+                onConveyorBelt = false;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (onConveyorBelt)
+            {
+                rigidbody.AddForce(conveyorBeltSpeed*conveyorBeltDirection*rigidbody.mass);
+            }
+        }
+    }
+}
