c9bc1651517c7d523f9b3c16c3b7bf8b7541cd57
Michael Grand <michael.grand.2@gmail.com>
2017-12-03 18:05:49 -0800

    Working on the snowfall effect. Probably broken: Don't pull.

--- Assets/Scripts/Snow.cs
+++ Assets/Scripts/Snow.cs
@@ -9,15 +9,47 @@
     {
         static Snow snowInstance;
 
+        [SerializeField]
+        RectTransform snowflakePrefab;
+
+        float lastTime;
+
+        float delay;
+
+        [SerializeField]
+        float minDelay = 0.5f;
+
+        [SerializeField]
+        float maxDelay = 2.0f;
+
+        Transform canvas;
+
+        const string guiTag = "GUI";
+
         void Awake()
         {
             if (snowInstance == null)
             {
                 DontDestroyOnLoad(this);
+                snowInstance = this;
             }
             else
             {
-                
+                DestroyObject(gameObject);
+            }
+
+            delay = Random.Range(minDelay, maxDelay);
+
+            canvas = GameObject.FindWithTag(guiTag).transform;
+        }
+
+        void Update()
+        {
+            if (Time.time >= lastTime + delay)
+            {
+                Instantiate(snowflakePrefab, new Vector3(Random.Range(-1000, 1000), 500), Quaternion.Euler(0, 0, Random.Range(0, 360.0f)), canvas);
+                lastTime = Time.time;
+                delay = Random.Range(minDelay, maxDelay);
             }
         }
     }
